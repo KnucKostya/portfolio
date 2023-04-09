370ca5ef60f907ce3276e7f30492f764372eac2a
KnucKostya <knuckostya@gmail.com>
2023-04-09 22:14:03 +0300

    09.04.2023 - mixins, vars Saas

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import s from './main.module.scss'
 import generalContainer from '../../common/styles/container.module.scss'
 import photo from '../../common/photo/img_portf.jpg'
+import ReactTypingEffect from 'react-typing-effect';
+
 
 const Main = () => {
     return (
@@ -10,15 +12,22 @@ const Main = () => {
                 <div className={s.textBlock}>
                     <span className={s.mainTitle}>Hello there</span>
                     <h1 className={s.nameTitle}>I'm Kostya Kokhanov,</h1>
-                    <p className={s.mainTitle}>I'm front-end developer with experience in creating SPA
-                        using React, Redux, Typescript, JavaScript, CSS, HTML.
-                        I've been good at absorbing information science childhood.
-                        I'm continuously improving my English studying new
-                        technologies and solving tasks on Codewars in my spare time.
-                        I also have analytical skills, responsibility,
-                        stress resistance, and initiative.
-                        Ready to consider project work and full-time employment.
-                    </p>
+                    <ReactTypingEffect
+                        text={"I'm front-end developer with experience in creating SPA"}
+                        className={s.mainTitle}
+                        speed={50}
+                        eraseSpeed={5}
+                        eraseDelay={2000}
+                    />
+                    {/*<p className={s.mainTitle}>I'm front-end developer with experience in creating SPA*/}
+                    {/*    using React, Redux, Typescript, JavaScript, CSS, HTML.*/}
+                    {/*    I've been good at absorbing information science childhood.*/}
+                    {/*    I'm continuously improving my English studying new*/}
+                    {/*    technologies and solving tasks on Codewars in my spare time.*/}
+                    {/*    I also have analytical skills, responsibility,*/}
+                    {/*    stress resistance, and initiative.*/}
+                    {/*    Ready to consider project work and full-time employment.*/}
+                    {/*</p>*/}
                 </div>
                 <div>
                     <img src={photo} className={s.ava} alt="portfolio_photo"/>
