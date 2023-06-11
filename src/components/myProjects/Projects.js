e72c3c95776c1fa87154ca0d9728ca8e56707b4c
My Name <knuckostya@gmail.com>
2023-06-11 16:15:23 +0300

    11.06.2023 - added new project, added new stack of tech

--- src/components/myProjects/Projects.js
+++ src/components/myProjects/Projects.js
@@ -5,6 +5,7 @@ import Project from "./Project/Project";
 import {Title} from "../../common/title/Title";
 import todoImage from '../../common/icons/todo-list.jpg'
 import socialNetworkImage from '../../common/icons/social-network.jpg'
+import cardsImage from '../../common/icons/cards.png'
 import counter from '../../common/icons/counter.JPG'
 import Fade from 'react-reveal/Fade';
 
@@ -21,22 +22,27 @@ const Projects = () => {
                     <Project
                         linkcode={'https://github.com/KnucKostya/redux-toolkit-todo.git'}
                         deploy={'https://knuckostya.github.io/redux-toolkit-todo/'}
-                        title={'Todo List'} description={'Stack: TS, RTK, Thunk, Unit-Test, API'}
+                        title={'Task Tracker'} description={'Stack: React, TS, RTK, Thunk, Material-UI, Unit-Test, API'}
                         img={todoImage}/>
+                    <Project
+                        linkcode={'https://github.com/KnucKostya/CardTraining.git'}
+                        deploy={'https://knuckostya.github.io/CardTraining/#/login'}
+                        title={'Learn words by cards'} description={'Stack: React, TS, RTK, Thunk, Material-UI, Unit-Test, API, Saas'}
+                        img={cardsImage}/>
                     <Project
                         linkcode={'https://github.com/KnucKostya/way-of-samurai-ts-upd.git'}
                         deploy={'https://knuckostya.github.io/way-of-samurai-ts-upd/'}
-                        title={'Social Network'} description={`Stack: TS, Redux, Thunk, Unit-Test, API HOC's`}
+                        title={'Social Network'} description={`Stack: React, TS, Redux, Thunk, Classes, OOP, Unit-Test, API HOC's`}
                         img={socialNetworkImage}/>
                     <Project
                         linkcode={'https://github.com/KnucKostya/rick_morty_api_pet_project.git'}
                         deploy={'https://knuckostya.github.io/rick_morty_api_pet_project/'}
-                        title={'Rick and Morty'} description={'Stack: TS, Redux, Thunk, Unit-Test, API'}
+                        title={'Rick and Morty'} description={'Stack: React, TS, Redux, Thunk, Unit-Test, API, Local Storage'}
                         img={'https://ae01.alicdn.com/kf/HTB1O8U9ANSYBuNjSspjq6x73VXaG/DC-418-14-rick-morty.jpg'}/>
                     <Project
                         linkcode={'https://github.com/KnucKostya/Counter.git'}
                         deploy={'https://knuckostya.github.io/Counter/'}
-                        title={'Counter'} description={'Stack: TS, Redux, Thunk, Unit-Test'}
+                        title={'Counter'} description={'Stack: React, TS, Redux, Thunk, Unit-Test, Local Storage'}
                         img={counter}/>
                 </div>
             </div>
